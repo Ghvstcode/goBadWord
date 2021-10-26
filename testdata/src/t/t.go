181501a41223d0df43d1c272a9a3cd0a9edf7639
Ghvstcode <impoissants@gmail.com>
2021-10-26 12:51:18 +0100

    Fixes

--- testdata/src/t/t.go
+++ testdata/src/t/t.go
@@ -1,1 +1,22 @@
 package t
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var thor string
+var hulk string
+
+func testtt(){
+	for {
+		go rand.Seed(time.Now().Unix())
+	}
+
+
+	thor = "newGreenEnergy"
+	sth := "swing"
+	fmt.Println(sth)
+}
+
